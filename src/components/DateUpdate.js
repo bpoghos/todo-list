6519d7ccd574d85b1338720c8a18e897226d3734
Poghos Barseghyan <bpoghos94@gmail.com>
2023-08-23 21:44:57 +0400

    create new component and import to the Header component

---
+++ src/components/DateUpdate.js
@@ -0,0 +1,9 @@
+const DateUpdate = () => {
+
+    return <div>
+        <small>Last Update 2023.08.23 21:42:34</small>
+    </div>
+}
+
+
+export default DateUpdate
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,11 @@
+import DateUpdate from "./DateUpdate"
+
 const Header = () =>{
-    return <h1>Hello World!</h1>
+    return <>
+    <h1>Hello World!</h1>
+    <div><DateUpdate /></div>
+    </>
+    
 }
 
 export default Header
