da419d27cd95babcbb442c82582f66f5d9a26e87
Poghos Barseghyan <bpoghos94@gmail.com>
2023-08-23 22:19:18 +0400

    update true code

--- src/components/DateUpdate.js
+++ src/components/DateUpdate.js
@@ -1,8 +1,9 @@
 const DateUpdate = () => {
 
-    return <div>
-        <small>Last Update 2023.08.23 21:42:34</small>
-    </div>
+    return (
+        <time>Last Update 2023.08.23 21:42:34</time>
+    )
+    
 }
 
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
 import DateUpdate from "./DateUpdate"
 
-const Header = () =>{
-    return <>
-    <h1>Hello World!</h1>
-    <div><DateUpdate /></div>
-    </>
-    
+const Header = () => {
+    return (<>
+        <h1>Hello World!</h1>
+        <DateUpdate />
+    </>)
+
 }
 
 export default Header
